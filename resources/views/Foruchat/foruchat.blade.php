e4293fc9f396bf054f6be8c07383aaa0e531e89f
Braicau Catalin <braicauc@yahoo.com>
2016-08-03 11:56:56 +0300

    Add AUTH_PRIVATE_KEY into default.blade.php

--- resources/views/Foruchat/foruchat.blade.php
+++ resources/views/Foruchat/foruchat.blade.php
@@ -24,6 +24,9 @@
                 <div class="row">
 
 
+
+
+
                 </div>
 
             </div>
--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -13,6 +13,12 @@
 
 <script>
     APP_PUBLIC_URL = '@yield('pURL')';
+    AUTH_USER_ID = '{{Auth::user()->id}}';
+@if(empty(\Illuminate\Support\Facades\Session::get('personalKey')))
+    AUTH_PRIVATE_KEY = '{{(new App\User())->setPersonalKey(Auth::user())}}';
+@else
+    AUTH_PRIVATE_KEY = '{{\Illuminate\Support\Facades\Session::get('personalKey')}}';
+@endif
 </script>
 
 <div class="wrapper">
